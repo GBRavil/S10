6a168f1fa87a37f2764bb4346ea787c0c2d19ac5
Ravil <lllx2@yandex.ru>
2022-11-28 14:36:32 +0300

    5

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -3,7 +3,7 @@ string[] array1 = new string[5] {"1234", "1567", "-2", "computer", "123"};
 int count = 0;
 string[] array2 = new string[ArrLen(count, array1)];
 FillArr(array1,array2);
-
+PrintArr(array2);
 
 int ArrLen(int count, string[] array)
 {
@@ -30,3 +30,9 @@ void FillArr(string[] array1, string[] array2)
     }
 }
 
+void PrintArr(string[] array2) {
+    for(int i =0; i<array2.GetLength(0); i++) {
+        Console.Write($"{array2 [i]} ");
+    }
+    Console.WriteLine();
+}
