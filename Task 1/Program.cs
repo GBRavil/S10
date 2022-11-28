a0efa5a87112565be051b539c42b19c6251da84b
Ravil <lllx2@yandex.ru>
2022-11-28 14:20:03 +0300

    3

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,2 +1,17 @@
 ﻿// Итоговая контрольная работа. 
 string[] array1 = new string[5] {"1234", "1567", "-2", "computer", "123"};
+int count = 0;
+string[] array2 = new string[ArrLen(count, array1)];
+
+int ArrLen(int count, string[] array)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+        count++;
+        }
+    }
+return count; 
+}
+
